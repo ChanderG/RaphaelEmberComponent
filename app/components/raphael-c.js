d40343dccea5ddea63033269f5803213dc6443d1
ChanderG <chandergovind@gmail.com>
2014-06-26 17:07:55 +0530

    Cleaned up, made basic api, multiple components positioning not exact

--- app/components/raphael-c.js
+++ app/components/raphael-c.js
@@ -11,23 +11,31 @@ export default Ember.Component.extend({
       height: self.get('height'),
       width: self.get('width')
     })); 
-    var circle = this.get('paper').raphael_canvas.circle(50, 40, 10);
+   
+    //use this in your raphael logic here   
+    var canvas = this.get('paper').raphael_canvas;
+
+    //simple convenience bindings
+    var height = this.get('height');  
+    var width = this.get('width'); 
+
+    var circle = canvas.circle(50, 40, 10);
     circle.attr("fill", "#f00");
     circle.attr("stroke", "#fff");
     
-    var circle1 = this.get('paper').raphael_canvas.circle(0, 0, 10);
+    var circle1 = canvas.circle(0, 0, 10);
     circle1.attr("fill", "#f00");
     circle1.attr("stroke", "#fff");
 
-    var circle2 = this.get('paper').raphael_canvas.circle(0, 100, 10);
+    var circle2 = canvas.circle(0, width, 10);
     circle2.attr("fill", "#f00");
     circle2.attr("stroke", "#fff");
 
-    var circle3 = this.get('paper').raphael_canvas.circle(100,0, 10);
+    var circle3 = canvas.circle(height ,0, 10);
     circle3.attr("fill", "#f00");
     circle3.attr("stroke", "#fff");
 
-    var circle4 = this.get('paper').raphael_canvas.circle(100,100, 10);
+    var circle4 = canvas.circle(height , width, 10);
     circle4.attr("fill", "#f00");
     circle4.attr("stroke", "#fff");
   }
